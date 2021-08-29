7916e9f22e66ecddc14beddc4b3c14b608ed6117
ericguizzo <eric.guizzo@hotmail.it>
2021-08-29 14:54:17 +0200

    cog-ified

--- predict.py
+++ predict.py
@@ -1,37 +1,35 @@
+import json
 import os
-import cog
 import tempfile
 from pathlib import Path
+
+import cog
+
 import speech_recognition as sr
-import json
+
 
 class RecognizeSpeech(cog.Predictor):
     def setup(self):
-        '''Init speech recognizer'''
+        """Init speech recognizer"""
         self.recognizer = sr.Recognizer()
 
     @cog.input("input", type=Path, help="Content image")
-
     def predict(self, input):
         """Transcript audio file using Sphinx engine"""
 
-        #read audio file
+        # read audio file
         with sr.AudioFile(str(input)) as source:
             audio = self.recognizer.record(source)
 
         # recognize speech using Sphinx
         try:
             recog = self.recognizer.recognize_sphinx(audio)
-            print("Sphinx thinks you said: " + recog)
+            #print("Sphinx thinks you said: " + recog)
         except sr.UnknownValueError:
             recog = "Sphinx could not understand audio"
-            print(recog)
+            #print(recog)
         except sr.RequestError as e:
             recog = "Sphinx error; {0}".format(e)
-            print(recog)
+            #print(recog)
 
-        output_path = Path(tempfile.mkdtemp()) / "output.txt"
-        with open(str(output_path), "w") as f:
-            f.write(recog)
-
-        return output_path
+        return recog
